7bad0ca78437bd12ee0518fb17f1fce5c49f134a
rchow1944 <rchow2@buffalo.edu>
2019-01-09 20:49:18 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+
+    @@genre_count[genre] ||= 0
+    @@genre_count[genre] += 1
+
+    @@artist_count[artist] ||= 0
+    @@artist_count[artist] += 1
+
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+  #
+  def self.artist_count
+    @@artist_count
+  end
+end
